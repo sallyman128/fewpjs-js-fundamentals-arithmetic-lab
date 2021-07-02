8405a3d6f7e146b9dbb5d67e7af0c19cff27dd65
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-02 07:37:33 -0400

    commit tree d32f01790d7a2b8d34687ee2e11cde060b372706

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 62;
+let num2 = 1;
+
+let multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+const num3 = 4;
+const num4 = 8;
+
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,20)
